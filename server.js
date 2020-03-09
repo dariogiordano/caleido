cd202224c9193710575e26f34ff89fd74bc8cb75
Dario Giordano <hello@dariogiordano.com>
2020-03-09 14:19:21 +0100

    again added express server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/caleido'));
 
 app.get('/*', function(req,res) {
     
@@ -14,3 +14,5 @@ res.sendFile(path.join(__dirname+'/dist/caleido/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+console.log("DONE")
